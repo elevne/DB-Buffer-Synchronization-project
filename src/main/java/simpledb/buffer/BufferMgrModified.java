cec3a45f2b14b54fb8320b302effa0989c5d14b5
elevne <cwi5525@naver.com>
2023-12-06 02:43:29 +0900

    DB modification project 진행 중

---
+++ src/main/java/simpledb/buffer/BufferMgrModified.java
@@ -0,0 +1,85 @@
+package simpledb.buffer;
+
+import simpledb.file.BlockId;
+import simpledb.file.FileMgr;
+import simpledb.log.LogMgr;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class BufferMgrModified {
+        private List<Buffer> bufferList; // List of unpinned buffers (LRU)
+        private Map<BlockId, Buffer> bufferMap; // Map of allocated buffers, keyed on block
+
+        private static final long MAX_TIME = 10000; // 10 seconds
+
+        public BufferMgrModified(FileMgr fm, LogMgr lm, int numbuffs) {
+            bufferList = new LinkedList<>();
+            bufferMap = new HashMap<>();
+            for (int i = 0; i < numbuffs; i++)
+                bufferList.add(new Buffer(fm, lm));
+        }
+
+        public synchronized int available() {
+            return bufferList.size();
+        }
+
+        public synchronized void flushAll(int txnum) {
+            for (Buffer buff : bufferMap.values())
+                if (buff.modifyingTx() == txnum)
+                    buff.flush();
+        }
+
+        public synchronized void unpin(Buffer buff) {
+            buff.unpin();
+            if (!buff.isPinned()) {
+                bufferList.add(buff); // Add to the end of the list (LRU)
+                notifyAll();
+            }
+        }
+
+        public synchronized Buffer pin(BlockId blk) {
+            try {
+                long timestamp = System.currentTimeMillis();
+                Buffer buff = tryToPin(blk);
+                while (buff == null && !waitingTooLong(timestamp)) {
+                    wait(MAX_TIME);
+                    buff = tryToPin(blk);
+                }
+                if (buff == null)
+                    throw new BufferAbortException();
+                return buff;
+            } catch (InterruptedException e) {
+                throw new BufferAbortException();
+            }
+        }
+
+        private boolean waitingTooLong(long starttime) {
+            return System.currentTimeMillis() - starttime > MAX_TIME;
+        }
+
+        private Buffer tryToPin(BlockId blk) {
+            Buffer buff = bufferMap.get(blk);
+            if (buff == null) {
+                buff = chooseUnpinnedBuffer();
+                if (buff == null)
+                    return null;
+                buff.assignToBlock(blk);
+                bufferMap.put(blk, buff); // Add to the map
+            }
+            if (!buff.isPinned())
+                bufferList.remove(buff); // Remove from the list (LRU)
+            buff.pin();
+            return buff;
+        }
+
+        private Buffer chooseUnpinnedBuffer() {
+            if (bufferList.isEmpty())
+                return null;
+            else
+                return bufferList.remove(0); // Remove from the head of the list (LRU)
+        }
+
+}
--- src/main/java/simpledb/tx/concurrency/LockTable.java
+++ src/main/java/simpledb/tx/concurrency/LockTable.java
@@ -1,103 +1,114 @@
 package simpledb.tx.concurrency;
 
-import java.util.*;
 import simpledb.file.BlockId;
+import simpledb.tx.concurrency.LockAbortException;
 
-/**
- * The lock table, which provides methods to lock and unlock blocks.
- * If a transaction requests a lock that causes a conflict with an
- * existing lock, then that transaction is placed on a wait list.
- * There is only one wait list for all blocks.
- * When the last lock on a block is unlocked, then all transactions
- * are removed from the wait list and rescheduled.
- * If one of those transactions discovers that the lock it is waiting for
- * is still locked, it will place itself back on the wait list.
- * @author Edward Sciore
- */
+import java.util.*;
+
 class LockTable {
-   private static final long MAX_TIME = 10000; // 10 seconds
-   
-   private Map<BlockId,Integer> locks = new HashMap<BlockId,Integer>();
-   
-   /**
-    * Grant an SLock on the specified block.
-    * If an XLock exists when the method is called,
-    * then the calling thread will be placed on a wait list
-    * until the lock is released.
-    * If the thread remains on the wait list for a certain 
-    * amount of time (currently 10 seconds),
-    * then an exception is thrown.
-    * @param blk a reference to the disk block
-    */
+   private static final long MAX_TIME = 10000;
+
+   private Map<BlockId, List<Integer>> locks = new HashMap<>();
+   private ThreadLocal<Integer> currentTxId = new ThreadLocal<>();
+
+   private Map<Integer, Integer> blkIdTxId = new HashMap<>();
+
    public synchronized void sLock(BlockId blk) {
       try {
+         int txId = generateTransactionId();
+         //currentTxId.set(txId);
+         blkIdTxId.put(blk.number(), txId);
          long timestamp = System.currentTimeMillis();
-         while (hasXlock(blk) && !waitingTooLong(timestamp))
+         while (hasXlock(blk) && !waitingTooLong(timestamp)) {
+            checkAbort(txId);
             wait(MAX_TIME);
+         }
          if (hasXlock(blk))
             throw new LockAbortException();
-         int val = getLockVal(blk);  // will not be negative
-         locks.put(blk, val+1);
-      }
-      catch(InterruptedException e) {
+         List<Integer> txids = locks.computeIfAbsent(blk, k -> new ArrayList<>());
+         //txids.add(getCurrentTxId());
+         txids.add(txId);
+      } catch (InterruptedException e) {
          throw new LockAbortException();
+      } finally {
+         currentTxId.remove();
       }
    }
-   
-   /**
-    * Grant an XLock on the specified block.
-    * If a lock of any type exists when the method is called,
-    * then the calling thread will be placed on a wait list
-    * until the locks are released.
-    * If the thread remains on the wait list for a certain 
-    * amount of time (currently 10 seconds),
-    * then an exception is thrown.
-    * @param blk a reference to the disk block
-    */
-   synchronized void xLock(BlockId blk) {
+
+   public synchronized void xLock(BlockId blk) {
       try {
+         int txId = generateTransactionId();
+         //currentTxId.set(txId);
+         blkIdTxId.put(blk.number(), txId);
          long timestamp = System.currentTimeMillis();
-         while (hasOtherSLocks(blk) && !waitingTooLong(timestamp))
+         while (hasOtherSLocks(blk) && !waitingTooLong(timestamp)) {
+            checkAbort(txId);
             wait(MAX_TIME);
+         }
          if (hasOtherSLocks(blk))
             throw new LockAbortException();
-         locks.put(blk, -1);
-      }
-      catch(InterruptedException e) {
+         //locks.put(blk, Collections.singletonList(-getCurrentTxId()));
+         locks.put(blk, Collections.singletonList(-txId));
+      } catch (InterruptedException e) {
          throw new LockAbortException();
+      } finally {
+         currentTxId.remove();
       }
    }
-   
-   /**
-    * Release a lock on the specified block.
-    * If this lock is the last lock on that block,
-    * then the waiting transactions are notified.
-    * @param blk a reference to the disk block
-    */
-   synchronized void unlock(BlockId blk) {
-      int val = getLockVal(blk);
-      if (val > 1)
-         locks.put(blk, val-1);
-      else {
-         locks.remove(blk);
-         notifyAll();
+
+   public synchronized void unlock(BlockId blk) {
+      try {
+         //int currentTxIdValue = getCurrentTxId();
+         int currentTxIdValue = blkIdTxId.get(blk.number());
+         List<Integer> txids = locks.get(blk);
+         if (txids != null) {
+            txids.remove(Integer.valueOf(currentTxIdValue));
+            if (txids.isEmpty()) {
+               locks.remove(blk);
+               notifyAll();
+            }
+         }
+      } catch (IllegalStateException e) {
+         e.printStackTrace();
       }
    }
-   
+
+   private void checkAbort(int currentTxid) {
+      //int currentTxid = getCurrentTxId();
+      for (List<Integer> txids : locks.values()) {
+         for (int heldTxid : txids) {
+            if (heldTxid < 0 && heldTxid < currentTxid) {
+               //System.out.println("Transaction " + getCurrentTxId() + " aborts");
+               throw new LockAbortException();
+            }
+         }
+      }
+   }
+
+   private int getCurrentTxId() {
+      Integer txId = currentTxId.get();
+      if (txId == null) {
+         throw new IllegalStateException("Transaction ID not set for the current thread.");
+      }
+      return txId;
+   }
+
+   private int generateTransactionId() {
+      // You may implement your logic to generate unique transaction IDs
+      return new Random().nextInt(1000) + 1; // For simplicity, using a random number here
+   }
+
    private boolean hasXlock(BlockId blk) {
-      return getLockVal(blk) < 0;
+      List<Integer> txids = locks.get(blk);
+      return txids != null && txids.stream().anyMatch(txid -> txid < 0);
    }
-   
+
    private boolean hasOtherSLocks(BlockId blk) {
-      return getLockVal(blk) > 1;
+      List<Integer> txids = locks.get(blk);
+      return txids != null && txids.size() > 1;
    }
-   
+
    private boolean waitingTooLong(long starttime) {
       return System.currentTimeMillis() - starttime > MAX_TIME;
-   }
-   
-   private int getLockVal(BlockId blk) {
-      Integer ival = locks.get(blk);
-      return (ival == null) ? 0 : ival.intValue();
    }
 }
